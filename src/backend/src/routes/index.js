7586bd7b8f7e6ba6d7cc9c9f96a1a3a9f45631f7
Israel2025 <israeldofuturo@outlook.com>
2025-12-01 19:50:16 -0300

    comfirmado: Corrigido variáveis

--- src/backend/src/routes/index.js
+++ src/backend/src/routes/index.js
@@ -5,12 +5,12 @@ const auth = require('./auth.routes');
 const products = require('./product.routes');
 const vendor = require('./vendor.routes');
 const admin = require('./admin.routes');
-const withdrawal = require('./withdrawal.routes');
+const withdrawal = require('./withdraw.routes');
 
 router.use('/auth', auth);
 router.use('/products', products);
 router.use('/vendor', vendor);
 router.use('/admin', admin);
-router.use('/withdrawals', withdrawal);
+router.use('/withdraw', withdrawal);
 
 module.exports = router;
